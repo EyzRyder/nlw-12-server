8bdb76031a594b4000614764751ad8d65214c2fb
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2023-07-17 14:00:07 -0300

    added upload route

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,36 @@
-import "dotenv/config";
-import fastify from "fastify";
-import cors from "@fastify/cors";
-import jwt from "@fastify/jwt";
-import { memoriesRoute } from "./routes/memories";
-import { authRoutes } from "./routes/auth";
-import { prisma } from "./lib/prisma";
-const port = 3333;
-const app = fastify();
+import "dotenv/config"
+
+import fastify from "fastify"
+import cors from "@fastify/cors"
+import jwt from "@fastify/jwt"
+import multipart from "@fastify/multipart"
+import { memoriesRoute } from "./routes/memories"
+import { authRoutes } from "./routes/auth"
+import { uploadRoutes } from "./routes/upload"
+import { resolve } from "node:path"
+
+const port = 3333
+
+const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true,
-});
+})
 
 app.register(jwt, {
   secret: "spacetime",
-});
+})
 
-app.get("/users", async () => {
-  const users = await prisma.user.findMany();
-  return users;
-});
-
-app.register(authRoutes);
-app.register(memoriesRoute);
+app.register(authRoutes)
+app.register(uploadRoutes)
+app.register(memoriesRoute)
 
 app
   .listen({
@@ -30,5 +38,5 @@ app
     host:'0.0.0.0'
   })
   .then(() => {
-    console.log(`✅🚄 Server running in http://localhost:${port}`);
-  });
+    console.log(`✅🚄 Server running in http://localhost:${port}`)
+  })
